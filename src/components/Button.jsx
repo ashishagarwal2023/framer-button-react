af4a4543be28bc9c7f34ba1b8d83cd74f248d5b1
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-20 10:43:30 +0000

    nice button

---
+++ src/components/Button.jsx
@@ -0,0 +1,39 @@
+import "./Button.css";
+import { motion } from "framer-motion"
+
+export const Button = ({gradBg = "250, 250, 250", solidBg = "15, 15, 15", overlay = "255, 255, 255", fontSize = "sm", children}) => {
+  return (
+    <motion.button
+      style={{
+        "--radial-gradient-background": gradBg,
+        "--solid-color-background": solidBg,
+        "--overlay-color": overlay
+      }}
+      initial={{ "--x": "100%", scale: 1 }}
+      animate={{ "--x": "-100%" }}
+      whileTap={{ scale: 0.97 }}
+      whileHover={{ scale: 1.03 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 1,
+        type: "spring",
+        stiffness: 20,
+        damping: 15,
+        mass: 2,
+        scale: {
+          type: "spring",
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
+      className="px-6 py-2 rounded-md relative radial-gradient"
+    >
+      <span className={`text-neutral-100 tracking-wide font-light h-full w-full block relative linear-mask text-${fontSize}`}>
+        {children}
+      </span>
+      <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
+    </motion.button>
+  )
+}
